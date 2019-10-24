65173dedc042bedac5320ea4e34fff04cdcadb4f
Kotikangas Tommi <tommi.kotikangas@solteq.com>
2019-10-24 12:54:57 +0300

    Initial commit

---
+++ src/main/java/com/example/tinyhouse/TinyhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyhouseApplication.class, args);
+	}
+
+}
