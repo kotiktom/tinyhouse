b7477c72f1fe2798e1ba8e75c7fa6d68b3a69810
Wanbro <aleksanteri.fagerholm@gmail.com>
2019-11-15 00:07:26 +0200

    Added couple of tests (hopefully junit doesnt fuck us again :D)

--- src/test/java/com/tinyhouse/TinyhouseApplicationTests.java
+++ src/test/java/com/tinyhouse/TinyhouseApplicationTests.java
@@ -2,12 +2,15 @@ package com.tinyhouse;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.tinyhouse.controller.QuestionController;
 import com.tinyhouse.model.Question;
 import com.tinyhouse.repository.QuestionRepository;
 
@@ -18,6 +21,8 @@ public class TinyhouseApplicationTests {
 	
 	@Autowired
 	private QuestionRepository questionRepository;
+    private QuestionController questionController;
+
  
 	@Test
     public void createQuestion() {
@@ -31,6 +36,23 @@ public class TinyhouseApplicationTests {
     	assertThat(q.getId()).isNotNull();
     	assertThat(q.getContent().contentEquals("Testi "));
     }
+	@Test
+    public void testQuestionList() {
+        List<Question> questions = questionRepository.findAll();
+        
+        assertThat(questions).hasSize(4);
+        assertThat(questions.get(0).getContent()).isEqualTo("Koulutusohjelma: ");
+    }
+	
+	@Test
+    public void DeletingAllQuestions() {
+		questionRepository.deleteAll();
+        assertThat(questionRepository.count()).isEqualTo(0);
+    }
  
+	 @Test
+	    public void contexLoads() throws Exception {
+	        assertThat(questionController).isNull();
+	    }
 
 }
