1be4c8bb69689474ae9a2f4243d90d838ee0e7ed
Kotikangas Tommi <tommi.kotikangas@solteq.com>
2019-11-26 13:56:21 +0200

    Changing POST to receive list of objects

--- src/main/java/com/tinyhouse/controller/AnswerController.java
+++ src/main/java/com/tinyhouse/controller/AnswerController.java
@@ -58,18 +58,24 @@ public class AnswerController {
 	@PostMapping("/questions/answers")
 	@ResponseBody
 	// TODO Muuta vastaus tulemaan itse pyynnön mukana
-	public Question postAnswer(@RequestBody ResponseAnswer answer){
-		Question q = questionRepository.getOne(answer.getQuestionid());
+	public Question postAnswer(@RequestBody ArrayList<ResponseAnswer> answer){
 		
-		Answer newAnswer = new Answer(answer.getAnswer(), q);
+		ArrayList<ResponseAnswer> responses = answer;
+
+		for (int i = 0; i < answer.size(); i++) {
+		Question q = questionRepository.getOne(answer.get(i).getQuestionid());
+		
+		Answer newAnswer = new Answer(answer.get(i).getAnswer(), q);
 		
 		ArrayList<Answer> newQuestionWithAnswer = new ArrayList<Answer>();
 		newQuestionWithAnswer.add(newAnswer);
 	
 		answerrepo.save(newAnswer);
-		q.getAnswers().add(newAnswer);		
+		q.getAnswers().add(newAnswer);	
+		}
 		
-		return q;
+		return null;
+		
 	}
 	
 	@GetMapping("/answers")
