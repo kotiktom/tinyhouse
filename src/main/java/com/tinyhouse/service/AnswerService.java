90b0ab4b91c9b4b769a3f6eb5f10282412eb62aa
mjHarakka <mj.harakka@gmail.com>
2019-11-26 13:36:15 +0200

    commit

--- src/main/java/com/tinyhouse/service/AnswerService.java
+++ src/main/java/com/tinyhouse/service/AnswerService.java
@@ -1,8 +1,13 @@
 package com.tinyhouse.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tinyhouse.model.Answer;
 import com.tinyhouse.model.Question;
@@ -16,6 +21,15 @@ public class AnswerService {
 	AnswerRepository answerRepository;
 	@Autowired
 	QuestionRepository questionRepository;
+	
+	@GetMapping("/questions/{id}/answers")
+	@ResponseBody
+	public List<Answer> getAnswers(@PathVariable long id){
+		
+		Question q = questionRepository.getOne(id);
+		
+		return q.getAnswers();
+	}
 	
 	@Transactional	
 	public void saveAnswer(long id, String answerContent) {
